37b431b85bbb0d79140209209acdbd8b19cffeba
Ross Mason <N00230645@iadt.ie>
2025-03-06 12:31:49 +0000

    deleted user table migration and created add user roles and image migration, and modified the script

--- database/migrations/2025_03_06_122403_add_role_and_image_to_users_table.php
+++ database/migrations/2025_03_06_122403_add_role_and_image_to_users_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('role', ['admn', 'campaigner', 'crowdfunder', 'investor'])->default('crowdfunder');
+            $table->string('image')->nullable();
         });
     }
 
@@ -22,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['role', 'image']);
+        });
     }
 };
